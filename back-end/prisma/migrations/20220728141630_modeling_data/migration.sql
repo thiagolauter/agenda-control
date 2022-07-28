e831d67aac28690cf4953b649d3cb4ec1dff27c8
Thiago Lauter <thiagolauterti@gmail.com>
2022-07-28 11:54:08 -0300

    data remodeling

--- back-end/prisma/migrations/20220728141630_modeling_data/migration.sql
+++ back-end/prisma/migrations/20220728141630_modeling_data/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
